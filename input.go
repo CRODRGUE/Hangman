79552e3ceaae5c7078d0bd2f3ed4e81d15c54af7
cyril <cyril.rodrigues@ynov.com>
2021-12-07 20:28:52 +0100

    update new fonction

--- input.go
+++ input.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-var letterToRune rune
+//var letterToRune rune
 
 func input() string {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -17,6 +17,46 @@ func input() string {
 	return choice
 }
 
+func (p *hangmanData) checkInput() {
+	letter := input()
+	letter = strings.ToLower(letter)
+	for _, letterToRune := range letter {
+		if !(('a' <= letterToRune && letterToRune <= 'z') || ('A' <= letterToRune && letterToRune <= 'Z')) {
+			fmt.Println(" --> Tu peux seulement mettre des carateres alphabetiques !! Recommence encore une fois l'ami... <--")
+			input()
+		}
+	}
+	if len(letter) != 1 {
+		if letter == p.wordToFind {
+			for index, char := range p.wordToFind {
+				p.word[index] = string(char)
+			}
+		} else {
+			p.counter -= 2
+			fmt.Println(" --> Tu t'es tromper l'ami !! Tu penser avoir le bon mot... <--")
+		}
+	} else {
+		check := p.duplicateEntry(letter)
+		if check {
+			fmt.Println(" --> Tu as deja entree cette lettre l'ami ! Ps: Change de lettre =) <-- ")
+		} else {
+			p.letterlist = append(p.letterlist, letter)
+			p.CheckLetter(strings.ToLower(letter))
+		}
+	}
+}
+
+func (p hangmanData) duplicateEntry(letter string) bool {
+	var checkL bool
+	for _, letterList := range p.letterlist {
+		if letter == string(letterList) {
+			checkL = true
+		}
+	}
+	return checkL
+}
+
+/*
 func (p *hangmanData) checkInput() {
 	letter := input()
 	letter = strings.ToLower(letter)
@@ -37,4 +77,4 @@ func (p *hangmanData) checkInput() {
 		p.letterlist = append(p.letterlist, letter)
 		p.CheckLetter(strings.ToLower(letter))
 	}
-}
+} */
